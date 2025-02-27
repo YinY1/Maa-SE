3f243f9f46bc54de73678eced4bb6351afb1f638
Kal'tsit <36351545+YinY1@users.noreply.github.com>
2025-02-27 16:01:41 +0800

    feat: 更新日志管理功能，添加动态日志级别支持

--- src-tauri/crates/app/src/lib.rs
+++ src-tauri/crates/app/src/lib.rs
@@ -2,39 +2,42 @@
 
 mod core;
 
-use core::{run_daily, stop_core, update_config};
-use std::sync::{Arc, Mutex};
+use core::{run_daily, set_log_level, stop_core, update_config};
+use std::sync::{Arc, Mutex, OnceLock};
 
 use anyhow::Context;
+use log4rs::{init_config, Handle};
 use maa_cfg::Config;
-use maa_core::tauri_logger::Logger;
-use tauri::AppHandle;
+use maa_core::tauri_logger::{log_config, LogHandleState};
+use tauri::{AppHandle, Manager};
 
 pub(crate) type CommandResult = Result<(), String>;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() -> anyhow::Result<()> {
     let config_state = Config::load(None).context("load configs")?;
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .manage(Arc::new(Mutex::new(config_state))) // TODO: 有没有必要不用mutex
+        .manage(LogHandleState(OnceLock::new()))
         .setup(|app| {
-            build_logger(app.handle().clone());
+            let log_handle = init_log(app.handle().clone())?;
+            app.state::<LogHandleState>().0.get_or_init(|| log_handle);
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
             run_daily,
             stop_core,
-            update_config
+            update_config,
+            set_log_level,
         ])
-        .manage(Arc::new(Mutex::new(config_state)))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
     Ok(())
 }
 
-fn build_logger(handle: AppHandle) {
-    let logger = Logger::new(handle);
-    env_logger::Builder::from_default_env()
-        .target(env_logger::Target::Pipe(Box::new(logger)))
-        .init();
+fn init_log(handle: AppHandle) -> anyhow::Result<Handle> {
+    let config = log_config(handle, log::LevelFilter::Info).context("get log config")?;
+    init_config(config).context("init log with config")
 }
